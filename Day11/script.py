1d64d74884dabb2bf4ccebe51e738652b6b9a8d6
spkl <seba.fischer@gmail.com>
2024-12-11 20:43:36 +0100

    add solution for day 11, part 2

--- Day11/script.py
+++ Day11/script.py
@@ -1,29 +1,40 @@
 import os
 os.chdir(os.path.dirname(__file__))
 
+TOTAL_BLINKS = 75
+solutions: dict[str, dict[int, int]] = {} # stone: blinks: resulting number of stones
+
 def main():
     with open('input.txt', 'r', encoding='utf-8') as f:
-        stones = f.read().strip().split()
+        original_stones = f.read().strip().split()
 
-    blinks = 25
-    for _ in range(blinks):
-        new_stones = []
-        for stone in stones:
-            if stone == '0':
-                new_stones.append('1')
-            elif len(stone) % 2 == 0:
-                new_stones.append(stone[:len(stone)//2])
-                right_stone = stone[len(stone)//2:]
-                while len(right_stone) > 1 and right_stone[0] == '0':
-                    right_stone = right_stone[1:]
-                new_stones.append(right_stone)
-            else:
-                new_stones.append(str(int(stone) * 2024))
-        stones = new_stones
+    n_stones = 0
+    for stone in original_stones:
+        n_stones += solve(stone, TOTAL_BLINKS)
+    print(n_stones)
 
-    print(len(stones))
+def solve(stone: str, blinks: int) -> int:
+    if blinks == 0:
+        return 1
 
-#import cProfile
+    if stone in solutions and blinks in solutions[stone]:
+        return solutions[stone][blinks]
+
+    if stone == '0':
+        solution = solve('1', blinks - 1)
+    elif len(stone) % 2 == 0:
+        left_stone = stone[:len(stone)//2]
+        right_stone = stone[len(stone)//2:]
+        while len(right_stone) > 1 and right_stone[0] == '0':
+            right_stone = right_stone[1:]
+        solution = solve(left_stone, blinks - 1) + solve(right_stone, blinks - 1)
+    else:
+        solution = solve(str(int(stone) * 2024), blinks - 1)
+
+    if stone not in solutions:
+        solutions[stone] = {}
+    solutions[stone][blinks] = solution
+    return solution
+
 if __name__ == '__main__':
-    #cProfile.run('main()')
     main()
